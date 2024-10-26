f8f9ec1209d32a4aeafc525868b2262f0b13eeac
Miguel Angel Perez Diaz <mapd0004@protonmail.com>
2024-10-26 20:15:49 +0200

    mejroas everywhere

--- src/app/help-me/help-me.component.ts
+++ src/app/help-me/help-me.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-help-me',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HelpMeComponent {
 
+  lottieOptions: AnimationOptions = {
+    path: 'assets/animations/corazon.json',
+  };
+
+  fotos = [
+    { url: 'assets/noche.jpg', alt: 'Foto 1', descripcion: 'Un recuerdo especial' },
+    { url: 'assets/picasso.jpg', alt: 'Foto 2', descripcion: 'Momentos inolvidables' },
+    { url: 'assets/vinci.png', alt: 'Foto 3', descripcion: 'Aventuras juntos' },
+  ];
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,20 +11,46 @@ import 'zone.js';
 export class HomeComponent {
 
   lottieOptions: AnimationOptions = {
-    path: 'assets/animations/cat.json', // Ruta de la animación Lottie JSON
+    path: 'assets/animations/cat.json',
   };
 
-  currentDay = 150;  // Día actual
-  totalDays = 450;  // Día final (por ejemplo, el día del examen)
+  totalDias: number;
+  diaActual: number;
+  porcentajeProgreso: number;
+  finalizado: boolean = false;
 
-  get progressPercentage(): number {
-    return (this.currentDay / this.totalDays) * 100;
+
+  constructor() {
+    const inicio = new Date(2024, 9, 30); // 30 de octubre de 2024
+    const fin = new Date(2025, 5, 30);    // 30 de junio de 2025
+
+    const tiempoTotal = fin.getTime() - inicio.getTime();
+    this.totalDias = Math.floor(tiempoTotal / (1000 * 3600 * 24));
+
+    const hoy = new Date();
+    const tiempoTranscurrido = hoy.getTime() - inicio.getTime();
+    this.diaActual = Math.floor(tiempoTranscurrido / (1000 * 3600 * 24)) + 1;
+
+    if(this.diaActual < 0){
+      this.diaActual = 0;
+    }
+
+    // Verificar si hemos alcanzado el último día
+    if (this.diaActual >= this.totalDias) {
+      this.diaActual = this.totalDias;
+      this.porcentajeProgreso = 100;
+      this.finalizado = true; // Indica que se ha alcanzado el día final
+    } else {
+      // Calcula el porcentaje normal en caso de no haber llegado aún al final
+      this.porcentajeProgreso = (this.diaActual / this.totalDias) * 100;
+    }
   }
+
+
 
   isLoading = true;
 
-  constructor() {}
-  
+
 }
 
 
