764aa758e74c66bd4bd19329f54c81b34f4d7018
Miguel Angel Perez Diaz <mapd0004@protonmail.com>
2024-10-30 23:39:28 +0100

    flecos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,10 +36,10 @@ export class AppComponent implements OnInit {
 }
 
 const recompensas = [
-  { nombre: 'Recompensa 1', disponible: true, canjeado: false, imagen: '/assets/cena.png', descripcion: "Es hora de arreglarse y salir a cenar.", temas_para_desbloquear: 15 },
-  { nombre: 'Recompensa 2', disponible: true, canjeado: false, imagen: '/assets/merienda.png', descripcion: "Pues que mejor que un merienda para amenizar la tarde.", temas_para_desbloquear: 3 },
+  { nombre: 'Recompensa 1', disponible: false, canjeado: false, imagen: '/assets/cena.png', descripcion: "Es hora de arreglarse y salir a cenar.", temas_para_desbloquear: 15 },
+  { nombre: 'Recompensa 2', disponible: false, canjeado: false, imagen: '/assets/merienda.png', descripcion: "Pues que mejor que un merienda para amenizar la tarde.", temas_para_desbloquear: 3 },
   { nombre: 'Recompensa 3', disponible: true, canjeado: false, imagen: '/assets/chuches.png', descripcion: "Premio de bienvenida. No hagas trampas con los siguientes premios.", temas_para_desbloquear: 0 },
   { nombre: 'Recompensa 4', disponible: false, canjeado: false, imagen: '/assets/cine.png', descripcion: "Es momento de disfrutar de una bolsa de palominas viendo una peli.", temas_para_desbloquear: 8 },
   { nombre: 'Recompensa 5', disponible: false, canjeado: false, imagen: '/assets/ropa.png', descripcion: "Aqui me equivoqué... Vamos de compras pero no te pases.", temas_para_desbloquear: 30 },
-  { nombre: 'Recompensa 6', disponible: false, canjeado: false, imagen: '/assets/motivacion.png', descripcion: "SORPRESA, sientate y disfruta.", temas_para_desbloquear: 23 },
+  { nombre: 'Recompensa 6', disponible: false, canjeado: false, imagen: '/assets/motivacion.png', descripcion: "SORPRESA, sientate y disfruta. Reenviame esta carta", temas_para_desbloquear: 23 },
 ];
